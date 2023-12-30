16f55e72ae887d78ea6e12c4a1fb2a248101a382
Aaxhirrr <anola133@asu.edu>
2023-12-30 10:56:16 -0700

    Enhance button hover states

---
+++ .ghost.ts
@@ -0,0 +1,44 @@
+// Fix binding issue in hero-card
+// Fix typo in app description
+// Fix bug in hero search filtering
+// Fix broken link in navbar
+// Enhance button hover states
+// Patch dropdown UI for responsiveness
+// Improve navbar layout
+// Adjust route guards logic
+// Add loading spinner to dashboard
+// Add loading spinner to dashboard
+// Add loading spinner to dashboard
+// Fix broken link in navbar
+// Improve accessibility in detail view
+// Fix binding issue in hero-card
+// Cleanup unused imports in app module
+// Update favicon and meta
+// Add pipe for hero title casing
+// Fix binding issue in hero-card
+// Fix binding issue in hero-card
+// Tweak margin and padding for grid
+// Add pipe for hero title casing
+// Add pipe for hero title casing
+// Add loading spinner to dashboard
+// Add NotFoundComponent for invalid routes
+// Fix typo in app description
+// Enhance button hover states
+// Fix broken link in navbar
+// Add NotFoundComponent for invalid routes
+// Enhance button hover states
+// Test hero service observable stream
+// Cleanup unused imports in app module
+// Fix bug in hero search filtering
+// Fix bug in hero search filtering
+// Add loading spinner to dashboard
+// Adjust route guards logic
+// Cleanup unused imports in app module
+// Fix binding issue in hero-card
+// Add NotFoundComponent for invalid routes
+// Add pipe for hero title casing
+// Update README with usage instructions
+// Refactor HeroService for clarity
+// Tweak margin and padding for grid
+// Fix broken link in navbar
+// dummy update 1300
